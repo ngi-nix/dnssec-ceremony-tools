82ccdcb7a24f492a57ca78e27f755fdea746092e
Mihai Fufezan <fufexan@pm.me>
2021-09-02 20:04:43 +0300

    flake.nix: init

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "DNSSEC Offline KSK Ceremony Tools";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "aarch64-linux" "i686-linux" "x86_64-linux" ];
+
+      forAllSystems = f:
+        nixpkgs.lib.genAttrs supportedSystems (system: f system);
+
+      nixpkgsFor = forAllSystems (system:
+        import nixpkgs {
+          inherit system;
+          overlays = [ self.overlay ];
+        });
+    in
+    {
+      overlay = final: prev: with prev; let
+        py = python3.withPackages (p: with p; [
+          hjson
+          pyyaml
+          python-pkcs11
+        ]);
+      in
+      {
+        dnssec-ceremony-tools = stdenv.mkDerivation rec {
+          pname = "dnssec-ceremony-tools";
+          version = "unstable-2021-02-25";
+
+          src = ./.;
+
+          dontConfigure = true;
+          dontBuild = true;
+
+          installPhase = ''
+            substituteInPlace oks.py \
+              --replace '/usr/bin/env python3' '${py}/bin/python3'
+            mkdir -p $out/bin
+            cp oks.py $out/bin
+          '';
+        };
+      };
+
+      packages = forAllSystems (system: {
+        inherit (nixpkgsFor.${system}) dnssec-ceremony-tools;
+      });
+
+      defaultPackage = forAllSystems (system:
+        self.packages.${system}.dnssec-ceremony-tools);
+    };
+}
